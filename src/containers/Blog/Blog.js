e75ef357100319434045d70f427c7599985d6d20
Raymond Sotto <raymondjosephsotto@gmail.com>
2020-01-20 21:29:26 -0500

    Used react-router-dom to add Rout, Link, NavLink, & Switch on the Navigation section

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
-import './Blog.css';
+import './Blog.module.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost'
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -14,13 +15,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink to="/" exact activeClassName="my-active">Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-              <Route path="/" exact component={Posts}/>
-              <Route path="/new-post" exact component={NewPost}/>
+               
+                <Route path="/" exact component={Posts}/>
+                <Switch>
+                <Route path="/new-post" exact component={NewPost}/>
+                <Route path="/:id" exact component={FullPost}/>
+                </Switch>
             </div>
         );
     }
